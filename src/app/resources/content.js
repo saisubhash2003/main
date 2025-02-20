3ccd763888e8d4682adfe736f62e2fa02adbd947
Kunal Kundanam <kunal1976prince@gmail.com>
2025-02-20 06:43:40 +0000

    content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -9,7 +9,7 @@ const person = {
     role:      'Design Engineer',
     avatar:    '/images/avatar.jpg',
     location:  'Asia/Jakarta',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-    languages: ['English', 'Bahasa']  // optional: Leave the array empty if you don't want to display languages
+    languages: ['English', 'Hindi', 'Telugu']  // optional: Leave the array empty if you don't want to display languages
 }
 
 const newsletter = {
