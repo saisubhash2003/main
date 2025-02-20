49dc706975c6827930d04e2f7afa59906aee9f8b
Kunal Kundanam <kunal1976prince@gmail.com>
2025-02-20 06:14:15 +0000

    content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -47,7 +47,7 @@ const home = {
     label: 'Home',
     title: `${person.name}'s Portfolio`,
     description: `Portfolio website showcasing my work as a ${person.role}`,
-    headline: <>Design engineer and builder</>,
+    headline: <>Bussiness Enthusiaist and Cyber Analystr</>,
     subline: <>I'm Selene, a design engineer at <InlineCode>FLY</InlineCode>, where I craft intuitive<br/> user experiences. After hours, I build my own projects.</>
 }
 
