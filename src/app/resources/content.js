34c684e43d82b58eb64d9958c883aedba82ae331
Kunal Kundanam <kunal1976prince@gmail.com>
2025-02-20 06:44:55 +0000

    content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -6,7 +6,7 @@ const person = {
     get name() {
         return `${this.firstName} ${this.lastName}`;
     },
-    role:      'Design Engineer',
+    role:      'Bussiness Enthusiaist and Cyber Analyst',
     avatar:    '/images/avatar.jpg',
     location:  'Asia/Jakarta',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
     languages: ['English', 'Hindi', 'Telugu']  // optional: Leave the array empty if you don't want to display languages
